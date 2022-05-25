70042026f26e8b9d67d1f205f544d23eb5fef686
Pedro Vasconcelos <me@pedrovasconcelos.com>
2022-05-25 09:15:55 +0100

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -3,7 +3,7 @@
 echo "Setting up your Mac..."
 
 # Check for Oh My Zsh and install if we don't have it
-if test ! $(which omz); then
+if test ! -d ~/.oh-my-zsh; then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
